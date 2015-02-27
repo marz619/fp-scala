8b4f5a3bb5085313754404e354433692db1f1d30
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2015-02-27 11:16:58 -0500

    Exercise 4.1 Option trait
    implements map and getOrElse using pattern matching
    flatMap, orElse and filter implemented using map and getOrElse

---
+++ src/main/scala/ca/ammaar/fpscala/chapters/c04/Option.scala
@@ -0,0 +1,31 @@
+package ca.ammaar.fpscala.chapters.c04
+
+import scala.{Option => _, Either => _} // hides the standard library implementations
+
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f).getOrElse(None)
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    this.map(Some(_)).getOrElse(ob)
+
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
+}
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+object Option {
+}
+
+
