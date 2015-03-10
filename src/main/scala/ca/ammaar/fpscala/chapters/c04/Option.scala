582b95f2d8ace294b2274cbbf92ee5e825e8c6f5
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2015-03-09 20:51:21 -0400

    Exercise 4.2 - calculating the variance of a sequence

--- src/main/scala/ca/ammaar/fpscala/chapters/c04/Option.scala
+++ src/main/scala/ca/ammaar/fpscala/chapters/c04/Option.scala
@@ -26,6 +26,20 @@ case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
 object Option {
+  /**
+   * Calculates the mean of a sequence of Doubles
+   */
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  /**
+   * Calculates the variance of a sequence of Doubles
+   */
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
+
 }
 
 
