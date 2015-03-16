65b918326150fad8f38392daa8083af57e8d80eb
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2015-03-16 10:38:28 -0400

    Exercise 4.3, 4.4, 4.5
    4.3 map2 => combines two Option values via a function f
    4.4 sequence => take a list of Option, convert it to an Option of list
    4.5 traverse => apply some function while walking a list of
        sequenceViaTraverse

--- src/main/scala/ca/ammaar/fpscala/chapters/c04/Option.scala
+++ src/main/scala/ca/ammaar/fpscala/chapters/c04/Option.scala
@@ -17,11 +17,18 @@ sealed trait Option[+A] {
     case Some(a) => Some(f(a))
   }
 
+  /**
+   * returns default if this is None, other
+   */
   def getOrElse[B >: A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
 
+  /**
+   * Tries to convert this via f into an Option[B] or
+   * None
+   */
   def flatMap[B](f: A => Option[B]): Option[B] =
     map(f).getOrElse(None)
 
@@ -50,11 +57,58 @@ object Option {
     else Some(xs.sum / xs.length)
 
   /**
-   * Calculates the variance of a sequence of Doubles
+   * Exercise 4.2 Calculates the variance of a sequence of Doubles
    */
   def variance(xs: Seq[Double]): Option[Double] = {
     mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
   }
+
+  /**
+   * Lift a function to be Option'd
+   */
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _.map(f)
+
+  def abs0: Option[Double] => Option[Double] = lift(math.abs)
+
+  /**
+   * Try function
+   */
+  def Try[A](a: => A): Option[A] =
+    try Some(a)
+    catch { case e: Exception => None }
+
+  /**
+   * Exercise 4.3 map2, that combines two Option values
+   */
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(sa => b.map(sb => f(sa, sb)))
+
+  /**
+   * Exercise 4.4 sequence
+   *
+   * Takes a List of Option(s) and via Option.flatMap
+   * converts it into an Option of List
+   */
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a match {
+      case Nil => Some(Nil)
+      case h :: t => h.flatMap(sh => sequence(t).map(sh :: _))
+    }
+
+  /**
+   * Exercise 4.5 traverse
+   */
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a match {
+      case Nil => Some(Nil)
+      case h :: t => f(h).flatMap(sh => traverse(t)(f).map(sh :: _))
+    }
+
+  /**
+   * Exercise 4.5 sequence via traverse
+   */
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(_.map(b => b))
 
 }
 
--- src/test/scala/ca/ammaar/fpscala/chapters/c04/Option$Test.scala
+++ src/test/scala/ca/ammaar/fpscala/chapters/c04/Option$Test.scala
@@ -8,6 +8,7 @@ import ca.ammaar.fpscala.chapters.util._
 class Option$Test extends FunSuite {
 
   val l: List[Double] = List(1.0, 2.0, 3.0, 4.0)
+  val ol: List[Option[Double]] = List(1.0, 2.0, 3.0, 4.0).map(Some(_))
 
   test("The mean of a sequence") {
     assert(mean(l).get ~= 2.5)
@@ -15,5 +16,12 @@ class Option$Test extends FunSuite {
 
   test("The variance of a sequence") {
     assert(variance(l).get ~= 1.25)
+  }
+
+  test("Sequence and Sequence Via Traverse") {
+    assert(sequence(List()).equals(Some(List())))
+    assert(sequence(ol).equals(Some(l)))
+    assert(sequenceViaTraverse(List()).equals(Some(List())))
+    assert(sequenceViaTraverse(ol).equals(Some(l)))
   }
 }
