ad955beb2ac4786879df5660b2fdf4a5112c830b
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2015-03-26 10:41:26 -0400

    Exercise 4.6 implementing map, flatMap, orElse and map2 for Either

--- src/main/scala/ca/ammaar/fpscala/chapters/c04/Either.scala
+++ src/main/scala/ca/ammaar/fpscala/chapters/c04/Either.scala
@@ -3,14 +3,45 @@ package ca.ammaar.fpscala.chapters.c04
 import scala.{Either => _} // hides the standard library implementation
 
 sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Right(a) => Right(f(a))
+      case Left(e) => Left(e)
+    }
 
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(e) => Left(e)
+      case Right(a) => f(a)
+    }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(_) => b
+      case Right(a) => Right(a)
+    }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      a <- this
+      eb <- b
+    } yield f(a, eb)
 }
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
 object Either {
-  def mean(xs: Seq[Double]): Either[String, Double] = {
+  def mean(xs: Seq[Double]): Either[String, Double] =
     if (xs.isEmpty) Left("mean of empty list!")
     else Right(xs.sum / xs.length)
-  }
+
+  def saveDiv(x: Int, y: Int): Either[Exception, Int] =
+    try Right(x / y)
+    catch { case e: Exception => Left(e) }
+
+  def safeDiv_1(x: Int, y: Int): Either[Exception, Int] = Try(x / y)
+
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e) }
 }
